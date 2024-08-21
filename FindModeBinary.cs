d20622411c422e21ac7a0b69b30a3946671924cf
eneas <ebaltazar@bside.com.mx>
2024-08-21 15:44:55 -0600

    Get Find model binarysearch

---
+++ FindModeBinary.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    internal class FindModeBinary
+    {
+
+        private int frequency = 0;
+        private int frequencyAnt = 0;
+        private int? ant = null;
+        private IList<int> result = new List<int>();
+
+        private void InOrder(TreeNode root)
+        {
+            if (root == null)
+            {
+                return;
+            }
+
+            InOrder(root.left);
+
+            //Get the same logic than in array
+            if (ant == root.val)
+            {
+                frequency++;
+            }
+            else
+            {
+                if (frequencyAnt < frequency)
+                {
+                    frequencyAnt = frequency;
+                    result.Clear();
+                    result.Add(ant ?? root.val);
+                }
+                else if (frequency == frequencyAnt)
+                {
+                    result.Add(ant ?? root.val);
+                }
+
+                frequency = 1;
+            }
+
+            ant = root.val;
+            InOrder(root.right);
+
+        }
+
+
+        public int[] FindMode(TreeNode root)
+        {
+            result.Clear();
+            frequency = 0;
+            frequencyAnt = 0;
+            ant = null;
+
+            InOrder(root);
+
+            if (ant == null)
+            {
+                return [.. result];
+            }
+
+            if (frequencyAnt < frequency)
+            {
+                result.Clear();
+                result.Add(ant.Value);
+            }
+            else if (frequencyAnt == frequency)
+            {
+                result.Add(ant.Value);
+            }
+
+
+            return [.. result];
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -4,5 +4,6 @@
 //var result = NNodes.RemoveNthFromEnd(new ListNode(1,new ListNode(2, new ListNode(3, new ListNode(4, new ListNode(5))))), 2);
 
 
-var result = KeyBoard.FindWords(["adsdf", "sfd"]);
+var find = new FindModeBinary();
+var result = find.FindMode(new TreeNode(0));
 Console.WriteLine(result);
