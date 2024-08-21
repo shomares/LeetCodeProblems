be7e0b30e17c41a465901662e465b347a29a5cde
eneas <ebaltazar@bside.com.mx>
2024-08-21 17:12:48 -0600

    Relative ranks

--- Program.cs
+++ Program.cs
@@ -4,6 +4,5 @@
 //var result = NNodes.RemoveNthFromEnd(new ListNode(1,new ListNode(2, new ListNode(3, new ListNode(4, new ListNode(5))))), 2);
 
 
-var find = new FindModeBinary();
-var result = find.FindMode(new TreeNode(0));
+var result = RelativeRanks.FindRelativeRanks([10, 3, 8, 9, 4]);
 Console.WriteLine(result);
---
+++ RelativeRanks.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    internal class RelativeRanks
+    {
+        public static string[] FindRelativeRanks(int[] score)
+        {
+            var result = new string[score.Length];
+            var hashmap = new Dictionary<int, int>();
+            var medals = new Dictionary<int, string> {
+                {1, "Gold Medal" },
+                {2, "Silver Medal" },
+                {3, "Bronze Medal" },
+
+            };
+
+            var index = 0;
+
+            foreach (int i in score)
+            {
+                hashmap.Add(i, index);
+                index++;
+            }
+
+            Array.Sort(score);
+
+            index = score.Length - 1;
+            while (index >= 0)
+            {
+                var scoreValue = score[index];
+
+                if (hashmap.TryGetValue(scoreValue, out var position))
+                {
+                    var relative = score.Length - index;
+                    result[position] = medals.TryGetValue(relative, out string? value) ? value : relative.ToString();
+                }
+
+                index--;
+            }
+
+
+            return result;
+
+        }
+    }
+}
